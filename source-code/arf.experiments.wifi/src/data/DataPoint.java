57cdf6ef3dcdb64740f226de76aca311949504b0
Jorge <josago@josago-laptop>
2013-06-17 12:04:18 +0200

    Implemented useful methods for DataPoints. Included the original datasets along with the convertion script used.

--- source-code/arf.experiments.wifi/src/data/DataPoint.java
+++ source-code/arf.experiments.wifi/src/data/DataPoint.java
@@ -1,23 +1,57 @@
 package data;
 
+import java.text.*;
+import java.util.*;
+
 public class DataPoint
 {
+	// Example time block sizes:
+	
+	public static final int TIME_BLOCK_SIZE_MINUTE = 60;
+	public static final int TIME_BLOCK_SIZE_HOUR   = 3600;
+	
+	// Format for the human-readable version of the "start" field:
+	
+	private static final SimpleDateFormat startDateFormat = new SimpleDateFormat("dd-mm-yyyy HH:mm:ss", Locale.ENGLISH);
+	
+	// Private data fields:
+	
 	public final int start, length, ID;
+	
+	private final Date startDate;
 	
 	DataPoint(int start, int length, int ID)
 	{
 		this.start  = start;
 		this.length = length;
 		this.ID     = ID;
+		
+		startDate = new Date(start * 1000L);
 	}
 	
+	/**
+	 * Returns the starting time of the event in a human-readable format.
+	 * @return The starting time of the event in a human-readable format.
+	 */
+	public String startDate()
+	{
+		startDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		
+		return startDateFormat.format(startDate);
+	}
+	
+	/**
+	 * Returns the starting time block of the event within the day. The first time block within a day has the number zero.
+	 * @param blockSize Size of the time blocks, in seconds.
+	 * @return The starting time block of the event within the day.
+	 */
+	@SuppressWarnings("deprecation")
 	public int startBlock(int blockSize)
 	{
-		return start / blockSize;
-	}
-	
-	public int lengthBlock(int blockSize)
-	{
-		return length / blockSize;
+		int mappedHour = (startDate.getHours() + 24 + startDate.getTimezoneOffset() / 60) % 24;
+		
+		int secondsElapsedDay = mappedHour * 3600 + startDate.getMinutes() * 60 + startDate.getSeconds();
+		
+		return secondsElapsedDay / blockSize;
 	}
 }
--- source-code/arf.experiments.wifi/src/data/Test.java
+++ source-code/arf.experiments.wifi/src/data/Test.java
@@ -2,6 +2,7 @@ package data;
 
 public class Test
 {
+	@SuppressWarnings("unused")
 	public static void main(String[] args)
 	{
 		HouseData houseA = new HouseData("houseA");		
@@ -9,5 +10,21 @@ public class Test
 		HouseData houseC = new HouseData("houseC");
 		HouseData houseD = new HouseData("houseD");
 		HouseData houseE = new HouseData("houseE");
+		
+		DataPoint[] data = houseA.sensorData(HouseData.MAPPING_LEVEL_FEATURE);
+		
+		// Example 1:
+		
+		for (DataPoint p: data)
+		{
+			System.out.println(p.startDate() + "\t" + p.length + "\t" + p.ID);
+		}
+		
+		// Example 2:
+		
+		for (DataPoint p: data)
+		{
+			System.out.println(p.startBlock(DataPoint.TIME_BLOCK_SIZE_MINUTE) + "\t" + p.length + "\t" + p.ID);
+		}
 	}
 }
